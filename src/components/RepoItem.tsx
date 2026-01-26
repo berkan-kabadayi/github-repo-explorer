49f1e38ead151969ae25d95225e18f7c0e0ee4f6
Berkan Kabadayı <kabadayiberkan@gmail.com>
2026-01-26 16:22:36 +0300

    feat: add RepoItem component with typed props

--- src/components/RepoItem.tsx
+++ src/components/RepoItem.tsx
@@ -1,8 +1,19 @@
-
-function RepoItem() {
-  return (
-    <div>RepoItem</div>
-  )
+interface RepoProps {
+  id: number;
+  name: string;
+  html_url: string;
+  description: string | null;
 }
 
-export default RepoItem
+function RepoItem({ repo }: { repo: RepoProps }) {
+  return (
+    <div>
+      <a href={repo.html_url} target="_blank" rel="noreferrer">
+        {repo.name}
+      </a>
+      <p>{repo.description || "No Explantion"}</p>
+    </div>
+  );
+}
+
+export default RepoItem;
