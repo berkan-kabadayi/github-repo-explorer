8102d95ce71875a6ff78944fd84edc04a6cb7749
Berkan Kabadayı <kabadayiberkan@gmail.com>
2026-01-26 16:54:47 +0300

    feat: add RepoList component to render repositories

--- src/components/RepoItem.tsx
+++ src/components/RepoItem.tsx
@@ -1,4 +1,4 @@
-interface RepoProps {
+export interface RepoProps {
   id: number;
   name: string;
   html_url: string;
--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,5 +1,26 @@
-function RepoList() {
-  return <div>RepoList</div>;
+import { Col, Row } from "react-bootstrap";
+import type { RepoProps } from "./RepoItem";
+import Card from "./Card";
+import RepoItem from "./RepoItem";
+
+interface RepoListProps {
+  repos: RepoProps[];
+}
+
+function RepoList({ repos }: RepoListProps) {
+  return (
+    <>
+      <Row xs={1} md={2} className="g-4">
+        {repos.map((repo) => (
+          <Col key={repo.id}>
+            <Card>
+              <RepoItem repo={repo} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
 export default RepoList;
